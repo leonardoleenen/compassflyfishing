1761f821fa7cfb2e78abb3a6ed0feae389304d30
Maria noel Leenen <marianoel.leenen@agtech.com.ar>
2019-04-29 09:09:09 -0300

    splash

--- webapp/pages/splash.vue
+++ webapp/pages/splash.vue
@@ -1,12 +1,11 @@
 <template v-slot:activator>
   <div class="welcome-section content-hidden">
         <div class="content-wrap">
-          <ul class="fly-in-text">
-            <li>C</li>
-            <li>F</li>
-          </ul>
-            <v-btn class="enter-button" @click="sheet = true">Enter</v-btn>
-        </div>
+          <div class="bb">
+             <h1  class="header" >CF</h1>
+          </div>
+           <v-btn outline  class="enter-button" @click="sheet = true">Enter</v-btn>
+       </div>
         <v-bottom-sheet v-model="sheet">
         <v-list>
           <v-subheader>Sign in with</v-subheader>
@@ -25,17 +24,97 @@
     margin: 0;
     padding: 0;
   }
+  .header{
+    padding-top: 30px;
+    font-size: 60px;
+    font-family: Pacifico;
+    letter-spacing: 17px;
+  }
+  .content-wrap{
+    padding:50px;
+  }
+  .v-btn.v-btn--outline{
+    color:#fff;
+    border:1px solid #fff;
+    margin: auto;
+    text-align: center;
+     margin-top: 74px;
+  }
+.bb, .bb::before, .bb::after {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.bb {
+  margin: auto;
+  color: #fff;
+  box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.5);
+  text-align: center;
+}
+.bb::before, .bb::after {
+  content: '';
+  z-index: -1;
+  margin: -5%;
+  box-shadow: inset 0 0 0 2px;
+  animation: clipMe 8s linear infinite;
+}
+.bb::before {
+  animation-delay: -4s;
+}
+.bb:hover::after, .bb:hover::before {
+  background-color: rgba(255, 0, 0, 0.3);
+}
+
+@keyframes clipMe {
+  0%, 100% {
+    clip: rect(0px, 220px, 2px, 0px);
+  }
+  25% {
+    clip: rect(0px, 2px, 220px, 0px);
+  }
+  50% {
+    clip: rect(218px, 220px, 220px, 0px);
+  }
+  75% {
+    clip: rect(0px, 220px, 220px, 218px);
+  }
+}
+html,
+body {
+  height: 100%;
+}
+
+body {
+  position: relative;
+  background-color: #0f222b;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
   body {
     font-family: 'Open Sans', Arial, sans-serif;
     font-weight: 700;
   }
-  .welcome-section {
+ .welcome-section {
     position: absolute;
     width: 100%;
     height: 100%;
     top: 0;
     left: 0;
-    background-color: #3B4252;
+   background: rgba(147,206,222,1);
+background: -moz-linear-gradient(top, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(2,119,189,1) 100%);
+background: -webkit-gradient(left top, left bottom, color-stop(0%, rgba(147,206,222,1)), color-stop(41%, rgba(117,189,209,1)), color-stop(100%, rgba(2,119,189,1)));
+background: -webkit-linear-gradient(top, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(2,119,189,1) 100%);
+background: -o-linear-gradient(top, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(2,119,189,1) 100%);
+background: -ms-linear-gradient(top, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(2,119,189,1) 100%);
+background: linear-gradient(to bottom, rgba(147,206,222,1) 0%, rgba(117,189,209,1) 41%, rgba(2,119,189,1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#93cede', endColorstr='#0277bd', GradientType=0 );
     overflow: hidden;
   }
   .welcome-section .content-wrap {
@@ -69,7 +148,6 @@
     .welcome-section .content-wrap .fly-in-text li {font-size: 10em; }
     .welcome-section .content-wrap .enter-button {font-size: 1.5em;}
   }
-
 </style>
 
 <script>
