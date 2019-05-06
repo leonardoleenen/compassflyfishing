c2e9e375630b4d0e1a0093051d91b655d0b8d730
Maria noel Leenen <marianoel.leenen@agtech.com.ar>
2019-05-06 17:24:42 -0300

    program list

--- webcomponents/src/components/fl-program-list/fl-program-list.tsx
+++ webcomponents/src/components/fl-program-list/fl-program-list.tsx
@@ -3,10 +3,36 @@ import { Component } from '@stencil/core';
 @Component({
   tag: 'fl-program-list',
   styleUrl: 'fl-program-list.scss',
-  shadow: true
 })
+
 export class FLProgramList {
   render() {
-    return <div>This is a program list</div>;
+    return <div>
+      <h2>
+        titulo
+      </h2>
+ 
+
+
+<div class="contain">
+  <div class="row">
+    <div class="row__inner">
+    <a class="arrow arrow-prev" href="#item-3"></a>
+    <a class="arrow arrow-next" href="#item-2"></a>
+
+      <div class="tile">
+       <fl-program-card></fl-program-card>
+      </div>
+      <div class="tile">
+       <fl-program-card></fl-program-card>
+      </div>
+      <div class="tile">
+       <fl-program-card></fl-program-card>
+      </div>
+  </div>
+  </div>
+
+</div>
+    </div>;
   }
 }
