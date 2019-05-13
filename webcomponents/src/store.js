91aa7b18fc6655ee1388827eef89aeba3e72b51f
Leonardo Gabriel Leenen <leonardo.leenen@agtech.com.ar>
2019-05-12 22:30:21 -0300

    Adding mobile scroll effect

--- webcomponents/src/store.js
+++ webcomponents/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { combineReducers } from 'redux'
 import thunk from 'redux-thunk'; // Add-on you might want
 import logger from 'redux-logger'; // Add-on you might want
